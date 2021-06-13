ab0ec3337f6baab00c8cd701e1091f7bccfd1b0e
amunyana <67626756+amunyana@users.noreply.github.com>
2021-06-13 02:37:31 -0700

    Update blue-green-script.js

--- blue-green-script.js
+++ blue-green-script.js
@@ -16,8 +16,8 @@ const songs = [
   {
     title: 'Blue, Green',
     artist: 'Maya-Marie',
-    coverPath: 'assets/images/Almiraj.png',
-    discPath: 'assets/music/blue&green.mp3',
+    coverPath: 'Almiraj.png',
+    discPath: 'blue&green.mp3',
     duration: '0:02',
   },
 ];
@@ -119,7 +119,7 @@ play.addEventListener('click', playPauseMedia);
 disc.addEventListener('play', updatePlayPauseIcon);
 disc.addEventListener('pause', updatePlayPauseIcon);
 disc.addEventListener('timeupdate', updateProgress);
-disc.addEventListener('ended', gotoNextSong.bind(null, true));
+disc.addEventListener('ended');
 
 // Go to next song when next button clicked
 prev.addEventListener('click', gotoPreviousSong);
